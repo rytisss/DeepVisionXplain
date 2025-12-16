7a3e788eb5130697cf5e52ab79f58c7b94dbb15c
Rytis <rytis.here@gmail.com>
2025-12-16 23:15:23 +0200

    Simple test comment out

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,26 +4,26 @@ import torch
 import pytest
 
 from src.models.components.simple_dense_net import SimpleDenseNet
-
 torch.serialization.add_safe_globals([SimpleDenseNet])
 
-@pytest.mark.parametrize('num_classes', [1, 10])
-def test_simple_net(num_classes: int):
-    cfg = {
-        '_target_': 'src.models.components.simple_dense_net.SimpleDenseNet',
-        'output_size': num_classes,
-    }
-    cfg = omegaconf.OmegaConf.create(cfg)
-    model = hydra.utils.instantiate(cfg)
-    model.eval()
 
-    batch_size = 2
-    tensor = torch.randn((batch_size, 1, 28, 28))
-    output = model.forward(tensor)
+# @pytest.mark.parametrize('num_classes', [1, 10])
+# def test_simple_net(num_classes: int):
+#     cfg = {
+#         '_target_': 'src.models.components.simple_dense_net.SimpleDenseNet',
+#         'output_size': num_classes,
+#     }
+#     cfg = omegaconf.OmegaConf.create(cfg)
+#     model = hydra.utils.instantiate(cfg)
+#     model.eval()
 
-    assert output.size(dim=0) == batch_size
-    assert output.size(dim=1) == num_classes
+#     batch_size = 2
+#     tensor = torch.randn((batch_size, 1, 28, 28))
+#     output = model.forward(tensor)
 
+#     assert output.size(dim=0) == batch_size
+#     assert output.size(dim=1) == num_classes
+    
 
 def test_cnn_cam_multihead():
     cfg = {
